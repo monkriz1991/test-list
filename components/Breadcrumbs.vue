7d5c9831ddce0e4693ca545e5beb790b2c8de102
mynmind <mynmind@proton.me>
2024-08-08 12:50:54 +0300

    scss

--- components/Breadcrumbs.vue
+++ components/Breadcrumbs.vue
@@ -10,32 +10,3 @@ defineProps({
     <div class="breadcrumbs__name">Профиль {{ profileName }}</div>
   </div>
 </template>
-
-<style lang="scss">
-.breadcrumbs {
-  display: flex;
-  margin: 20px 0;
-  align-items: center;
-  gap: 20px;
-
-  &__title {
-    font-size: 13px;
-    position: relative;
-    &::before {
-      content: "";
-      width: 3px;
-      height: 3px;
-      border-radius: 50%;
-      background-color: #333;
-      position: absolute;
-      right: -11px;
-      top: 8px;
-    }
-  }
-
-  &__name {
-    font-size: 13px;
-    color: #333;
-  }
-}
-</style>
